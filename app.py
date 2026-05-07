db1dae522de78ae7ff7fc51b3a25135606a03e88
dwij0016 <dwijthakur0016@gmail.com>
2026-05-07 17:32:08 +0530

    update func

--- app.py
+++ app.py
@@ -1,21 +1,47 @@
 def book():
     contact = {}
+    s = list(contact)
+    # print(s)
     print('----your contact book----')
     while True:
         print('available options')
-        print('1.add\n2.update\n3.delete\n4.viewn\n5.exit\nenter the opration = ')
+        print('1.add\n2.update\n3.delete\n4.viewn\n5.exit')
+        operation = (int(input('enter the operationt = ')))
         def a():
             key = input('name the contact = ')
             value = int(input('add the no. of the contact = '))
-            contact[key] = value
-            print(contact)
+            s.append(key)
+            s.append(value)
+            print(s)
+            
 
         def u():
             d = input('enter the key name want to update =')
             f = int(input('enter the no. you want to update = '))
-            contact.index(key) = d
-            contact.index(value) = f
-            print(f'the name is successfully updated as {d} and the no. as {f}')
-            print(contact)
+            b = input('enter the new name you want to update = ')
+            c = int(input('enter the new no. you want to update = '))
+            ind = s.index(d)
+            inx = s.index(f)
+            s[ind] = b
+            s[inx] = c
+            print(f'the name is successfully updated as {b} and the no. as {c}')
+            print(s)
 
-        
+        def de():
+            de_val_name = input('enter the name you want to delete =')
+            de_val_no = int(input('enter the no. you want to delete = '))
+            ix = s.index(de_val_name)
+            del s[ix]
+            io = s.index(de_val_no)
+            del s[io]
+            print(s)
+
+        if operation == 1:
+            a()
+        elif operation == 2:
+            u()
+        elif operation == 3:
+            de()
+        else:
+            print('jkgfkjd')
+book()
